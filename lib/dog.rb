bdbe9654a4a9b37981f8ff891a968ad03ed180d9
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-01 10:36:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,16 @@
 class Dog
   @@all = []
-  attr_accessor : name
+  attr_accessor :name
 
   def initialize(name)
     @name = name
     @@all << self
   end
+
   def self.clear_all
     @@all.clear
   end
   def self.all
-    @@all.each{|dog| puts "#{dog}"}
+    @@all.each {|dog| puts "#{dog}"}
   end
 end
